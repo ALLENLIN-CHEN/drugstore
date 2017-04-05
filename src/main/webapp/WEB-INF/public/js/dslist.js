9aaeabacb5ae2dd67076d4b233d7fd4bf111d683
crh5914 <249683003@qq.com>
2017-04-05 12:00:03 +0800

    列表展示数据填充

--- src/main/webapp/WEB-INF/public/js/dslist.js
+++ src/main/webapp/WEB-INF/public/js/dslist.js
@@ -6,6 +6,12 @@ $(function(){
     'people':null,
     'times':null
   };
+  var flash = {
+    'people':false,
+    'times':false,
+    'sum':false,
+    'avg':false
+  }
   var spinner = $('.spinner')[0];
   $(spinner).hide();
   $('.sub-item-wrap').on('click',clicklistener);
@@ -24,6 +30,8 @@ $(function(){
          }
   }
   function success(res){
+    console.log(res);
+    data[role] = res;
     update();
   }
   function getData(url){
@@ -37,11 +45,74 @@ $(function(){
      list();
   }
   function list(){
-
+     var tbhtml = '';
+     if(role=='people'){
+        showpeople();
+     }else if(role=='times'){
+       showtimes();
+     }else if(role=='avg'){
+       showavg();
+     }else{
+       showsum();
+     }
+  }
+  function showpeople(){
+        if(!flash[role]){
+           var tbhtml = "";
+           var item = null;
+           var peoplelist = data[role]
+           for(var i=0;i<peoplelist.length;i++){
+             item = peoplelist[i];
+             tbhtml = tbhtml + "<tr><td>"+item.year+"</td><td>"+item.freq+"</td><td>"+item.rare+"</td></tr>"
+           }
+           $('#people .table tbody').html(tbhtml);
+        }
+        $('#people').show();
+  }
+  function showtimes(){
+     if(!flash[role]){
+            var tuple = null;
+            var tbhtml = "";
+            var times = data[role]
+            for(key in times){
+                tuple = times[key];
+                tbhtml = tbhtml+"<tr><td>"+key+"</td><td>"+tuple['item1']+"</td><td>"+tuple['item2']+"</td><td>"+tuple['item3']+"</td><td>"+tuple['item4']+"</td><td>"+tuple['item5']+"</td><td>"+tuple['item6']+"</td><td>"+tuple['item7']+"</td></tr>"
+            }
+            $('#times .table tbody').html(tbhtml);
+            flash[role] = true;
+       }
+      $('#times').show();
+  }
+  function showavg(){
+     if(!flash[role]){
+         var tbhtml = "";
+         var tuple = null;
+         var avglist = data[role]
+         for(key in avglist){
+             tuple = avglist[key];
+             tbhtml = tbhtml+"<tr><td>"+key+"</td><td>"+tuple['item1']+"</td><td>"+tuple['item2']+"</td><td>"+tuple['item3']+"</td><td>"+tuple['item4']+"</td><td>"+tuple['item5']+"</td><td>"+tuple['item6']+"</td><td>"+tuple['item7']+"</td></tr>"
+         }
+          // console.log(tbhtml);
+         $('#avg .table tbody').html(tbhtml);
+         flash[role] = true;
+      }
+     $('#avg').show();
+  }
+  function showsum(){
+     if(!flash[role]){
+       var tbhtml = "";
+       var sumlist = data[role]['consum'];
+       for(var i=0;i<sumlist.length;i++){
+           tbhtml = tbhtml + "<tr><td>"+sumlist[i].year+"</td>"+"<td>"+sumlist[i].sum+"</td></tr>";
+       }
+      $('#sum .table tbody').html(tbhtml);
+      flash[role] = true;
+     }
+     $('#sum').show();
   }
   function pagelistener(event)
   {
-
+     $('#people').show();
   }
   function updatepage()
   {
